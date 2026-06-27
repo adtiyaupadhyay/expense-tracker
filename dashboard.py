e003142655326ea674b26e9e067ff3cd8a7536a9
Aditya Upadhyay <141498716+adtiyaupadhyay@users.noreply.github.com>
2026-06-28 03:49:15 +0530

    Change app.run to app.run_server in main block
    Updated app.run to app.run_server for better clarity.

--- dashboard.py
+++ dashboard.py
@@ -491,4 +491,4 @@ def run_dashboard(auto_open=True, port=8050):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8050, debug=False)
+    app.run_server(host='0.0.0.0', port=8050)  # Not 127.0.0.1
